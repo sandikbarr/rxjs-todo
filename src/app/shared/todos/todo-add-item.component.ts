502220a8dc9ec1e7633fd877ddee18deda198144
Sandi Barr <sandi.barr@prx.org>
2019-06-22 22:10:28 -0500

    clear new todo input on add

--- src/app/shared/todos/todo-add-item.component.ts
+++ src/app/shared/todos/todo-add-item.component.ts
@@ -1,11 +1,18 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-todo-add-item',
   template: `
-    <input #todo/><button (click)="addTodo.emit({description: todo.value})">Add</button>
+    <input #todo (change)="onAddTodo(todo.value)"/>
+    <button (click)="onAddTodo(todo.value)">Add</button>
   `
 })
 export class TodoAddItemComponent {
   @Output() addTodo = new EventEmitter();
+  @ViewChild('todo') input: ElementRef;
+
+  onAddTodo(description) {
+    this.addTodo.emit({description});
+    this.input.nativeElement.value = '';
+  }
 }
