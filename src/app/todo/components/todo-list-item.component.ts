7e241c617c75a34d7af30ebe4e71a341cec5a67e
Sandi Barr <sandi.barr@prx.org>
2019-06-24 11:03:50 -0500

    show tags in list

--- src/app/todo/components/todo-list-item.component.ts
+++ src/app/todo/components/todo-list-item.component.ts
@@ -6,18 +6,23 @@ import { Router } from '@angular/router';
   selector: 'app-todo-list-item',
   template: `
     <li>
-      <span>
-        <input
-          [id]="todo.id"
-          type="checkbox"
-          [checked]="todo.completed"
-          (click)="toggleTodo.emit(!todo.completed)"/>
-        <label [for]="todo.id">{{ todo.description }}</label>
-      </span>
-      <span>
-        <button (click)="onEditTodo()">Edit</button>
-        <button (click)="deleteTodo.emit()">Delete</button>
-      </span>
+      <div class="actions">
+        <span>
+          <input
+            [id]="todo.id"
+            type="checkbox"
+            [checked]="todo.completed"
+            (click)="toggleTodo.emit(!todo.completed)"/>
+          <label [for]="todo.id">{{ todo.description }}</label>
+        </span>
+        <span>
+          <button (click)="onEditTodo()">Edit</button>
+          <button (click)="deleteTodo.emit()">Delete</button>
+        </span>
+      </div>
+      <div *ngIf="todo.tags" class="tags">
+        <span class="tag" *ngFor="let tag of todo.tags">{{tag}}</span>
+      </div>
     </li>
   `,
   styleUrls: ['todo-list-item.component.css']
