daef3c5ef1bccf1ee87d036decfb0ff22a53d943
Sandi Barr <sandi.barr@prx.org>
2019-07-11 10:41:36 -0500

    use base

--- src/app/doby/http/doby-http.service.ts
+++ src/app/doby/http/doby-http.service.ts
@@ -30,14 +30,14 @@ export class DobyHttpService {
   }
 
   saveDoby(newDoby: Doby): Observable<Doby> {
-    return this.http.post<Doby>('http://localhost:3000/dobys', newDoby, {headers}).pipe(share());
+    return this.http.post<Doby>(this.base, newDoby, {headers}).pipe(share());
   }
 
   deleteDoby(deletedDoby: Doby): Observable<{}> {
-    return this.http.delete('http://localhost:3000/dobys/' + deletedDoby.id, {headers}).pipe(share());
+    return this.http.delete(`${this.base}/${deletedDoby.id}`, {headers}).pipe(share());
   }
 
   updateDoby(updatedDoby: Doby): Observable<Doby> {
-    return this.http.put<Doby>('http://localhost:3000/dobys/' + updatedDoby.id, updatedDoby, {headers}).pipe(share());
+    return this.http.put<Doby>(`${this.base}/${updatedDoby.id}`, updatedDoby, {headers}).pipe(share());
   }
 }
